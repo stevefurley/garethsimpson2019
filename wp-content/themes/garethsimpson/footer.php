5b5aaf9dd03cce69621acec0fb04aa12f5df6521
stevesnow100 <stevefurley@gmail.com>
2019-04-09 13:54:01 +0100

    updating contact frm styking

--- wp-content/themes/garethsimpson/footer.php
+++ wp-content/themes/garethsimpson/footer.php
@@ -17,7 +17,7 @@
         <span class='step step-2' attr-step='step-2'><span class='text'>Step 2</span><span class='dot'></span></span>
         <span class='step step-3' attr-step='step-3'><span class='text'>Step 3</span><span class='dot'></span></span>
       </div>
-      <h4 class='text-center font-600 pad-bottom-40 no-margin'>Ready to do some real SEO with an industry veteran?</h4>
+      <h4 class='text-center font-600 no-margin'>Ready to do some real SEO with an industry veteran?</h4>
       <div class='custom-form'>
         <?php echo do_shortcode( '[contact-form-7 id="264" title="Contact form 1"]' ); ?>
       </div>
